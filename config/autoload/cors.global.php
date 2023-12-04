d5a0dc0e91694b7a59ab2b9494c8bd9307dbeffc
Bakhtiyar Gizzat <1@recoursia.com>
2023-12-04 10:23:38 +0600

    fix cors politics

--- config/autoload/cors.global.php
+++ config/autoload/cors.global.php
@@ -10,7 +10,6 @@ return [
         'allowed_headers' => [], // No custom headers allowed
         'allowed_max_age' => '600', // 10 minutes
         'credentials_allowed' => true, // Allow cookies
-        'allowed_methods' => ['GET', 'POST'], // Allow any origin
         'exposed_headers' => ['X-Custom-Header'], // Tell client that the API will always return this header
     ],
 ];
