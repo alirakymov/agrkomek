be703e9d3fefb9ccd918447b00874db6cb2074bb
Bakhtiyar Gizzat <bakhtiyar.gizzat@mpsolutions.kz>
2024-02-19 19:24:34 +0600

    Notification firebase details

--- src/App/src/SynapseNodes/Components/Notification/NotificationFirebase.php
+++ src/App/src/SynapseNodes/Components/Notification/NotificationFirebase.php
@@ -2,6 +2,11 @@
 
 namespace Qore\App\SynapseNodes\Components\Notification;
 
+use Kreait\Firebase\Exception\MessagingException;
+use Kreait\Firebase\Exception\Messaging\InvalidMessage;
+use Kreait\Firebase\Exception\Messaging\NotFound;
+use Kreait\Firebase\Exception\Messaging\ServerError;
+use Kreait\Firebase\Exception\Messaging\ServerUnavailable;
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Firebase\Messaging\Notification;
@@ -34,8 +39,14 @@ class NotificationFirebase extends JobAbstract implements JobInterface
      */
     public function process() : bool
     {
+        /** @var ModelManager */
         $mm = Qore::service(ModelManager::class);
+        /** @var SynapseManager */
         $sm = Qore::service(SynapseManager::class);
+
+        # - Reconnect to mysql server
+        $connection = $mm->getAdapter()->getDriver()->getConnection();
+        $connection->connect();
 
         $notification = $mm('SM:Notification')->where(['@this.id' => $this->task['id']])->one();
         if (! $notification) {
@@ -44,22 +55,25 @@ class NotificationFirebase extends JobAbstract implements JobInterface
 
         $user = $mm('SM:User')->with('devices')->where(['@this.id' => $notification->idUser])->one();
 
-        $serviceAccount = PROJECT_DATA_PATH . '/firebase/google-services.json';
-        $firebase = (new Factory)->withServiceAccount($serviceAccount)->createMessaging();
-
-        $notify = Notification::create($notification->title, $notification->message);
-
         try {
+            $serviceAccount = PROJECT_DATA_PATH . '/firebase/google-services.json';
+            $factory = (new Factory)->withServiceAccount($serviceAccount);
+            $firebase = $factory->createMessaging();
+
+            $notify = Notification::create($notification->title, $notification->message);
+
             foreach ($user->devices() as $device) {
-                $message = CloudMessage::new()
+                $message = CloudMessage::withTarget('token', $device['token'])
                     ->withNotification($notify)
                     ->withData(array_merge($notification['data'], ['event' => $notification['event']]))
-                    ->withTarget('token', $device['token']);
-                $firebase->send($message);
+                    ->withHighestPossiblePriority();
+                $result = $firebase->send($message);
             }
         } catch (\Throwable $t) {
             dump($t);
         }
+
+        $connection->disconnect();
 
         return true;
     }
